5c0b3151d459c93739732b1e2e206c76731de1f9
Presti <dxssucuk@hotmail.com>
2023-06-06 20:12:06 +0200

    Funny test shit.

--- Trainer/test.py
+++ Trainer/test.py
@@ -1,42 +1,31 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
+import urllib.request
 import tensorflow as tf
-
 from tensorflow import keras
-from keras import layers
-from keras.models import Sequential
-import pathlib
 
 # Images get scaled to this:
 img_height = 256
 img_width = 256
 
-class_names = ['dream', 'nondream']
+class_names = ['nondream', 'dream']
 
-model = keras.models.load_model('../datasets/trained.h5')
+# Step 1: Download the image from the URL
+image_url = 'https://images.pexels.com/photos/1831234/pexels-photo-1831234.jpeg'
+image_path = 'test.jpg'  # Specify the path to save the downloaded image
+urllib.request.urlretrieve(image_url, image_path)
 
-img = keras.utils.load_img(
-    './validation/nondream/24754615-1-2-1.jpeg', target_size=(img_height, img_width)
-)
+# Step 2: Load and preprocess the downloaded image
+img = keras.preprocessing.image.load_img(image_path, target_size=(img_height, img_width))
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
-plt.imshow(img)
+# Step 3: Load the trained model
+model = keras.models.load_model('../datasets/model.h5')
 
-img_array = keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)
-
+# Step 4: Make predictions on the test image
 predictions = model.predict(img_array)
 score = predictions[0]
-scoreSoft = tf.nn.softmax(predictions[0])
+print(score)
+class_index = int(score >= 0.75)  # Assuming threshold of 0.5 for binary classification
+class_label = class_names[class_index]  # Replace with your class labels
 
-if score == 0:
-    plt.xlabel('Dream')
-elif score == 1:
-    plt.xlabel('Not Dream')
-
-plt.show()
-print(
-    "This is {}, {:.2f} percent confidence."
-    .format(class_names[int(score)], 100 * np.max(scoreSoft))
-)
+print("Predicted class label:", class_label)
--- Trainer/train.py
+++ Trainer/train.py
@@ -1,120 +1,61 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import SGD
-from keras import layers
-from keras.models import Sequential
-import pathlib
-
-data_dir = pathlib.Path('./image')
-
-batch_size = 32
+# Images get scaled to this:
 img_height = 256
 img_width = 256
+batch_size = 32
+num_epochs = 30
 
-# configure tensorflow to use gpu cuda
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_virtual_device_configuration(
-        physical_devices[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * 9)])
-    logical_devices = tf.config.experimental.list_logical_devices('GPU')
-    print(len(physical_devices), "Physical GPUs,",
-          len(logical_devices), "Logical GPUs")
-
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
+# Step 1: Load and preprocess your image dataset
+train_ds = keras.preprocessing.image_dataset_from_directory(
+    'image',
+    validation_split=0.2,
+    subset='training',
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+val_ds = keras.preprocessing.image_dataset_from_directory(
+    'validation',
+    validation_split=0.2,
+    subset='validation',
+    image_size=(img_height, img_width),
+    batch_size=batch_size
 )
 
-test_datagen = ImageDataGenerator(
-    rescale=1. / 255
-)
+# Step 2: Configure the dataset for performance
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-train_ds = train_datagen.flow_from_directory(
-    data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary'
-)
-
-val_ds = test_datagen.flow_from_directory(
-    './validation/',
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary'
-)
-
-model = Sequential([
-    # OLD NETWORK
-    #    layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-    #    layers.Conv2D(16, 3, padding='same', activation='relu'),
-    #    layers.MaxPooling2D(),
-    #    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    #    layers.MaxPooling2D(),
-    #    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    #    layers.MaxPooling2D(),
-    layers.Conv2D(32, (3, 3), activation='relu',
-                  input_shape=(img_height, img_width, 3)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(128, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(128, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
+# Step 3: Create your model
+model = keras.Sequential([
+    layers.Rescaling(1.0 / 255),  # Normalize pixel values to [0, 1]
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
     layers.Flatten(),
-    layers.Dense(512, activation='relu'),
-    layers.Dropout(0.5),
-    layers.Dense(1, activation='sigmoid')
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid')  # Output layer with sigmoid activation for binary classification
 ])
 
+# Step 4: Compile and train your model
 model.compile(optimizer='adam',
-              loss='binary_crossentropy',
+              loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
 
-model.summary()
-
-epochs = 30
-
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
+# Step 5: Create a ModelCheckpoint callback to save the model during training
+checkpoint_callback = keras.callbacks.ModelCheckpoint(
+    filepath='../datasets/model.h5',  # Specify the path to save the model
+    save_best_only=True,
+    monitor='val_loss',
+    mode='min',
+    verbose=1
 )
 
-acc = history.history['accuracy']
-loss = history.history['loss']
-
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.legend(loc='lower right')
-plt.title('Training Accuracy & Loss')
-plt.show()
-
-model.save('../datasets/trained.h5')
-print("[i] Model saved!")
-
-# Quick test of the model, use test.py for the sex
-
-img = tf.keras.utils.load_img(
-    './validation/dream/6733b8d5-63e9-47cf-91f6-2726e9bafaea.png', target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create a batch
-
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-
-class_names = ['dream', 'nondream']
-
-print(
-    "This is {}, {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score)))
+# Step 6: Train your model with the ModelCheckpoint callback
+model.fit(train_ds, validation_data=val_ds, epochs=num_epochs, callbacks=[checkpoint_callback])
